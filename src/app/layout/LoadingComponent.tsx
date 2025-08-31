fbbc3f40ebee48d3317b95ded8dc70464f8f43e2
Peter John Casasola <petercasasola@mysale.com>
2025-08-31 22:09:11 +0800

    Added Loading Compoment

---
+++ src/app/layout/LoadingComponent.tsx
@@ -0,0 +1,17 @@
+import { Dimmer, Loader } from "semantic-ui-react"
+
+type Props = {
+    inverted?: boolean
+    content?: string
+}
+
+
+const LoadingComponent = ({inverted = true, content = 'Loading .....'}: Props) => {
+  return (
+    <Dimmer active inverted={inverted}>
+      <Loader content={content} />
+    </Dimmer>
+  )
+}
+
+export default LoadingComponent
