a3fb262d57aa8ec73852192cd4f2d4611093d794
Peter John Casasola <petercasasola@mysale.com>
2025-08-31 22:10:31 +0800

    Implement Querying data from Firestore and Loading Component

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,19 +1,57 @@
-import { Grid } from "semantic-ui-react";
-import EventList from "./EventList";
-import { useAppSelector } from "@/app/store";
-
+import { Grid } from "semantic-ui-react"
+import EventList from "./EventList"
+import { useAppDispatch, useAppSelector } from "@/app/store"
+import { useEffect, useState } from "react"
+import {
+  collection,
+  onSnapshot,
+  query
+} from "firebase/firestore"
+import { db } from "@/config/firebase"
+import type { AppEvent } from "@/types/event"
+import { setEvents } from "../eventSlice"
+import LoadingComponent from "@/app/layout/LoadingComponent"
 
 export default function EventDashboard() {
+  const {events} = useAppSelector((state) => state.events)
+  const [isLoading, setIsLoading] = useState(true)
 
-  const {events} = useAppSelector((state) => state.events);
-  
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const q = query(collection(db, "events"))
+    const unsubscribe = onSnapshot(q, {
+      next: (querySnapshot) => {
+        const eventDocs: AppEvent[] = []
+        querySnapshot.forEach((doc) => {
+          eventDocs.push({ id: doc.id, ...doc.data() } as AppEvent)
+        })
+        
+        dispatch(setEvents(eventDocs))
+        setIsLoading(false)
+      },
+      error: (err) => {
+        console.error("Error fetching events: ", err)
+        setIsLoading(false)
+      },
+      complete: () => {
+        console.log("Event fetching complete")
+        setIsLoading(false)
+      }
+    })
+
+    return () => unsubscribe()
+
+  }, [dispatch])
+
+  if (isLoading) return <LoadingComponent />
+
   return (
     <Grid>
       <Grid.Column width={10}>
         <EventList events={events} />
       </Grid.Column>
-      <Grid.Column width={6}>
-    </Grid.Column>
+      <Grid.Column width={6}></Grid.Column>
     </Grid>
   )
 }
