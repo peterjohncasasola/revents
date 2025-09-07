c6dc4d9836c94056dc5813e1888320788f3796dd
Peter John Casasola <ptrjhndev@gmail.com>
2025-09-07 19:41:32 +0800

    Updated the auth listener hook to add route navigate

--- src/app/hooks/useAuthListener.ts
+++ src/app/hooks/useAuthListener.ts
@@ -3,13 +3,21 @@ import { auth } from "@/config/firebase"
 import { login, logout } from "@/features/auth/authSlice"
 import { useAppDispatch } from "@/app/store"
 import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { AppRoutes } from "@/app/router/AppRoutes"
 export const useAuthListener = () => {
   const dispatch = useAppDispatch()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) dispatch(login(user))
-      else dispatch(logout())
+      if (user) {
+        dispatch(login(user))
+        navigate(AppRoutes.Events)
+      } else {
+        dispatch(logout())
+        navigate(AppRoutes.Home)
+      }
     })
     return unsubscribe
   }, [dispatch])
